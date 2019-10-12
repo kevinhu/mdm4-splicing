849c334739e32d657d0c5c4e63bba59f8c71c2c8
Kevin Hu <kevinhuwest@gmail.com>
2019-10-12 18:10:52 -0400

    Suppa diffs

--- scripts/suppa_diff.sh
+++ scripts/suppa_diff.sh
@@ -11,26 +11,49 @@ SPLICE_TYPES=("A3" "A5" "AF" "AL" "MX" "RI" "SE")
 
 for s in ${SPLICE_TYPES[@]}; do
 
-# 	suppa.py psiPerEvent --ioe-file "${INDEX_PATH}/suppa_index_ioe_${s}_strict.ioe" \
-# 		--expression-file $TPM_MATRIX \
-# 		-o "${IOE_OUTPUT_PATH}/${s}"
-
-	echo "${INDEX_PATH}/suppa_index_ioe_${s}_strict.ioe"
-	echo "${PSI_PATH}/${s}_RPL22_oe_c.psi"
-	echo "${PSI_PATH}/${s}_RPL22_oe_t.psi"
-	echo "${TPM_PATH}/RPL22_oe_c.txt"
-	echo "${TPM_PATH}/RPL22_oe_t.txt"
-
 	python3 /Users/khu/Desktop/SUPPA-master/suppa.py diffSplice --method empirical \
 		--input "${INDEX_PATH}/suppa_index_ioe_${s}_strict.ioe" \
 		--psi "${PSI_PATH}/${s}_RPL22_oe_c.psi" "${PSI_PATH}/${s}_RPL22_oe_t.psi" \
 		--tpm "${TPM_PATH}/RPL22_oe_c.txt" "${TPM_PATH}/RPL22_oe_t.txt" \
 		--alpha 0.05 \
-		--lower-bound 0.01 \
-		--tpm-threshold 0.1 \
+		--lower-bound 0 \
+		--tpm-threshold 0 \
 		--nan-threshold 0 \
 		--area 1000 \
 		-o "${DIFF_PATH}/${s}_RPL22_oe"
+
+	python3 /Users/khu/Desktop/SUPPA-master/suppa.py diffSplice --method empirical \
+		--input "${INDEX_PATH}/suppa_index_ioe_${s}_strict.ioe" \
+		--psi "${PSI_PATH}/${s}_RPL22L1_oe_c.psi" "${PSI_PATH}/${s}_RPL22L1_oe_t.psi" \
+		--tpm "${TPM_PATH}/RPL22L1_oe_c.txt" "${TPM_PATH}/RPL22L1_oe_t.txt" \
+		--alpha 0.05 \
+		--lower-bound 0 \
+		--tpm-threshold 0 \
+		--nan-threshold 0 \
+		--area 1000 \
+		-o "${DIFF_PATH}/${s}_RPL22L1_oe"
+
+	python3 /Users/khu/Desktop/SUPPA-master/suppa.py diffSplice --method empirical \
+		--input "${INDEX_PATH}/suppa_index_ioe_${s}_strict.ioe" \
+		--psi "${PSI_PATH}/${s}_shluc.psi" "${PSI_PATH}/${s}_sh704.psi" \
+		--tpm "${TPM_PATH}/shluc.txt" "${TPM_PATH}/sh704.txt" \
+		--alpha 0.05 \
+		--lower-bound 0 \
+		--tpm-threshold 0 \
+		--nan-threshold 0 \
+		--area 1000 \
+		-o "${DIFF_PATH}/${s}_sh704"
+
+	python3 /Users/khu/Desktop/SUPPA-master/suppa.py diffSplice --method empirical \
+		--input "${INDEX_PATH}/suppa_index_ioe_${s}_strict.ioe" \
+		--psi "${PSI_PATH}/${s}_shluc.psi" "${PSI_PATH}/${s}_sh705.psi" \
+		--tpm "${TPM_PATH}/shluc.txt" "${TPM_PATH}/sh705.txt" \
+		--alpha 0.05 \
+		--lower-bound 0 \
+		--tpm-threshold 0 \
+		--nan-threshold 0 \
+		--area 1000 \
+		-o "${DIFF_PATH}/${s}_sh705"
 
 done
 
--- scripts/suppa_psi.sh
+++ scripts/suppa_psi.sh
@@ -4,6 +4,7 @@ INDEX_PATH="raw/suppa_indices"
 TPM_MATRIX="processed/tpm_matrix_suppa.txt"
 IOI_OUTPUT_PATH="intermediate/suppa_transcript_psis"
 IOE_OUTPUT_PATH="intermediate/suppa_exon_psis"
+GTF_PATH="raw/kallisto_homo_sapiens/Homo_sapiens.GRCh38.96.gtf"
 
 SPLICE_TYPES=("A3" "A5" "AF" "AL" "MX" "RI" "SE")
 
@@ -14,3 +15,7 @@ for s in ${SPLICE_TYPES[@]}; do
 		-o "${IOE_OUTPUT_PATH}/${s}"
 
 done
+
+suppa.py psiPerIsoform -g $GTF_PATH \
+	--expression-file $TPM_MATRIX \
+	-o "intermediate/suppa_isoforms"
