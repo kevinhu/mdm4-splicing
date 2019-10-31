cf498fd77836007750c19ad61a534555f577343c
Kevin Hu <kevinhuwest@gmail.com>
2019-10-31 18:27:50 -0400

    Remove SUPPA scripts

--- scripts/suppa_index.sh
+++
@@ -1,8 +0,0 @@
-cd ../data/raw/kallisto_homo_sapiens
-
-suppa.py generateEvents -i Homo_sapiens.GRCh38.96.gtf \
-	-o ../suppa_indices/suppa_index_ioe -f ioe \
-	-e SE SS MX RI FL --pool-genes
-
-suppa.py generateEvents -i Homo_sapiens.GRCh38.96.gtf \
-	-o ../suppa_indices/suppa_index_ioi -f ioi --pool-genes
