8993894cbb43e57b707d6ab60accf4e50cba5823
Kevin Hu <kevinhuwest@gmail.com>
2019-10-31 19:49:01 -0400

    Update 1_sleuth.R

--- notebooks/1_sleuth.R
+++ notebooks/1_sleuth.R
@@ -19,29 +19,29 @@ experiment_setup <- read.table(file.path("../experiment_setup.txt"), header = TR
 experiment_setup <- dplyr::mutate(experiment_setup, path = experiment_setup$sample)
 
 # make experiment summaries
-RPL22_oe_setup <- experiment_setup[c(1,2,3,4,5,6),]
-RPL22L1_oe_setup <- experiment_setup[c(7,8,9,10,11,12),]
+rpl22_oe_setup <- experiment_setup[c(1,2,3,4,5,6),]
+rpl22l1_oe_setup <- experiment_setup[c(7,8,9,10,11,12),]
 sh704_setup <- experiment_setup[c(13,14,15,16,17,18),]
 sh705_setup <- experiment_setup[c(13,14,15,19,20,21),]
 
 # import Sleuth objects
-RPL22_oe_so <- sleuth_prep(RPL22_oe_setup, extra_bootstrap_summary = TRUE, read_bootstrap_tpm=TRUE)
-RPL22L1_oe_so <- sleuth_prep(RPL22L1_oe_setup, extra_bootstrap_summary = TRUE, read_bootstrap_tpm=TRUE)
+rpl22_oe_so <- sleuth_prep(rpl22_oe_setup, extra_bootstrap_summary = TRUE, read_bootstrap_tpm=TRUE)
+rpl22l1_oe_so <- sleuth_prep(rpl22l1_oe_setup, extra_bootstrap_summary = TRUE, read_bootstrap_tpm=TRUE)
 sh704_so <- sleuth_prep(sh704_setup, extra_bootstrap_summary = TRUE, read_bootstrap_tpm=TRUE)
 sh705_so <- sleuth_prep(sh705_setup, extra_bootstrap_summary = TRUE, read_bootstrap_tpm=TRUE)
 
 # likelihood ratio tests
-RPL22_oe_so <- sleuth_fit(RPL22_oe_so, ~condition, 'full')
-RPL22_oe_so <- sleuth_fit(RPL22_oe_so, ~1, 'reduced')
-RPL22_oe_so <- sleuth_lrt(RPL22_oe_so, 'reduced', 'full')
-RPL22_oe_table <- sleuth_results(RPL22_oe_so, 'reduced:full', 'lrt', show_all = TRUE)
-write.table(RPL22_oe_table, file = "../kallisto_sleuth/RPL22_oe.csv", sep=",",col.names=TRUE, row.names=TRUE)
+rpl22_oe_so <- sleuth_fit(rpl22_oe_so, ~condition, 'full')
+rpl22_oe_so <- sleuth_fit(rpl22_oe_so, ~1, 'reduced')
+rpl22_oe_so <- sleuth_lrt(rpl22_oe_so, 'reduced', 'full')
+rpl22_oe_table <- sleuth_results(rpl22_oe_so, 'reduced:full', 'lrt', show_all = TRUE)
+write.table(rpl22_oe_table, file = "../kallisto_sleuth/rpl22_oe.csv", sep=",",col.names=TRUE, row.names=TRUE)
 
-RPL22L1_oe_so <- sleuth_fit(RPL22L1_oe_so, ~condition, 'full')
-RPL22L1_oe_so <- sleuth_fit(RPL22L1_oe_so, ~1, 'reduced')
-RPL22L1_oe_so <- sleuth_lrt(RPL22L1_oe_so, 'reduced', 'full')
-RPL22L1_oe_table <- sleuth_results(RPL22L1_oe_so, 'reduced:full', 'lrt', show_all = TRUE)
-write.table(RPL22L1_oe_table, file = "../kallisto_sleuth/RPL22L1_oe.csv", sep=",",col.names=TRUE, row.names=TRUE)
+rpl22l1_oe_so <- sleuth_fit(rpl22l1_oe_so, ~condition, 'full')
+rpl22l1_oe_so <- sleuth_fit(rpl22l1_oe_so, ~1, 'reduced')
+rpl22l1_oe_so <- sleuth_lrt(rpl22l1_oe_so, 'reduced', 'full')
+rpl22l1_oe_table <- sleuth_results(rpl22l1_oe_so, 'reduced:full', 'lrt', show_all = TRUE)
+write.table(rpl22l1_oe_table, file = "../kallisto_sleuth/rpl22l1_oe.csv", sep=",",col.names=TRUE, row.names=TRUE)
 
 sh704_so <- sleuth_fit(sh704_so, ~condition, 'full')
 sh704_so <- sleuth_fit(sh704_so, ~1, 'reduced')
@@ -58,12 +58,10 @@ write.table(sh705_table, file = "../kallisto_sleuth/sh705.csv", sep=",",col.name
 # add Ensembl gene to transcript mappings
 mart <- biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL",
                          host="feb2014.archive.ensembl.org",
-                         dataset = "hsapiens_gene_ensembl",
-                         host = "ensembl.org")
+                         dataset = "hsapiens_gene_ensembl")
 
 t2g <- biomaRt::getBM(attributes = c("ensembl_transcript_id", 
                                      "ensembl_gene_id",
-                                     "external_gene_name",
                                      "ensembl_peptide_id",
                                      "hgnc_symbol"
                                      ), mart = mart)
