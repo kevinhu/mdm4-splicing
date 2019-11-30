a634e2bb0fbb3db22d23bcde9924c6e25aa1e6c4
Kevin Hu <kevinhuwest@gmail.com>
2019-11-30 00:06:48 -0500

    Higher resolution figure PNGs

--- figures/pdf-to-png.sh
+++ figures/pdf-to-png.sh
@@ -3,9 +3,11 @@
 #convert PDFs to PNGs for preview in Google docs
 
 for i in *.pdf; do
+
+    echo $i
     
-  name=$i;
-  name=${name%.*};
-  sips -s format png $i --out ${name}.png;
+    name=$i;
+    name=${name%.*};
+    convert -density 512 -quality 100 -colorspace RGB $i ${name}.png;
 
 done
