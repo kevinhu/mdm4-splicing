560c1e3b7655fbe3081008877637d26ebea3d449
Kevin Hu <kevinhuwest@gmail.com>
2020-02-02 19:35:06 -0500

    Update 5_intron_cluster.sh

--- scripts/5_intron_cluster.sh
+++ scripts/5_intron_cluster.sh
@@ -2,4 +2,4 @@
 
 cd /Volumes/aulyd_xk/leafcutter_juncs/
 
-python /Users/khu/Desktop/github/leafcutter/clustering/leafcutter_cluster.py -j juncfiles.txt -m 50 -o testYRIvsEU -l 500000
+python /Users/khu/Desktop/github/leafcutter/clustering/leafcutter_cluster.py -j juncfiles.txt -m 50 -o leafcutter_clusters -l 500000
