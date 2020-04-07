c4532769b2882f2a1668f8f00fdd5aa204ace56c
Kevin Hu <kevinhuwest@gmail.com>
2020-04-06 21:40:55 -0400

    Removed leafcutter scripts

--- scripts/5_intron_cluster.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /Volumes/aulyd_xk/leafcutter_juncs/
-
-python /Users/khu/Desktop/github/leafcutter/clustering/leafcutter_cluster.py -j juncfiles.txt -m 50 -o leafcutter_clusters -l 500000
